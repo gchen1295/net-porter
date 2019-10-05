1bdef9dd4ac138aa08460a5e559ed63cd1815094
Woof <gchen1258@gmail.com>
2019-10-04 22:08:08 -0400

    Finalized nap and frontend bot

--- app/nap.js
+++ app/nap.js
@@ -341,7 +341,7 @@ async function getSizes(productURL, proxy)
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
       },
-      //proxy: agent,
+      proxy: agent,
       resolveWithFullResponse: true,
       followAllRedirects: true
     })
@@ -564,7 +564,7 @@ async function getAllProductsAPI(proxy)
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
       },
-      //proxy: agent,
+      proxy: agent,
       resolveWithFullResponse: true,
       followAllRedirects: true
     })
